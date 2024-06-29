789ab7ac4ec66a7a2473c53378990bcfd8d2ec2e
Tristan <daelune@gmail.com>
2024-06-29 12:58:28 +0100

    Bind providers

--- src/app/Infrastructure/Providers/AppServiceProvider.php
+++ src/app/Infrastructure/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Infrastructure\Providers;
 
+use App\Domain\Repositories\AssigneeRepository;
+use App\Domain\Repositories\DatePeriodRepository;
+use App\Domain\Repositories\ProjectRepository;
+use App\Infrastructure\Repositories\EloquentAssigneeRepository;
+use App\Infrastructure\Repositories\EloquentDatePeriodRepository;
+use App\Infrastructure\Repositories\EloquentProjectRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +17,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(ProjectRepository::class, EloquentProjectRepository::class);
+        $this->app->bind(AssigneeRepository::class, EloquentAssigneeRepository::class);
+        $this->app->bind(DatePeriodRepository::class, EloquentDatePeriodRepository::class);
     }
 
     /**
